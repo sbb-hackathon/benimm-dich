811832db923aa48116affe85d4279d00d92ffad3
Verena Ursula Esther Irrgang <v.irrgang@campus.tu-berlin.de>
2019-08-29 14:04:50 +0200

    access of quotes fitting to hashtag

--- reply.py
+++ reply.py
@@ -4,6 +4,7 @@ import tweepy
 import logging
 from config import create_api
 import time
+import json
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -16,8 +17,19 @@ def check_mentions(api, keywords, since_id):
         new_since_id = max(tweet.id, new_since_id)
         if tweet.in_reply_to_status_id is not None:
             continue
-        if any(keyword in tweet.text.lower() for keyword in keywords):
-            logger.info(f"Answering to {tweet.user.name}")
+        if (keyword in tweet.text.lower() for keyword in keywords):
+            logger.info("Answering to {tweet.user.name}")
+            
+            #get all jsons with certain hashtag
+            with open('data.json') as json_file:
+                data = json.load(json_file)
+                idxs = []
+                i=0
+                for i,d in enumerate(data):
+                    if keyword in d['hashtags']:
+                            idxs=i
+                            
+            #random
 
             if not tweet.user.following:
                 tweet.user.follow()
