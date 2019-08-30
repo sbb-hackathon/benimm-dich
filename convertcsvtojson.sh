d45ccc7e36d769fe60cde3b2bb00deb40edfeb3d
Isabel <isabel@tuberlin.com>
2019-08-30 11:51:23 +0200

    updated scrip und data

--- convertcsvtojson.sh
+++ convertcsvtojson.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 items=( )
-while IFS=';' read -r id cont auth title year link hash grading _; do
-  printf -v item '{ "cont": %s,\n "auth": %s,\n "title": %s,\n "year": %s,\ "link": %s,\n "hash": {%s},\n }\n' "$cont" "$auth" "$title" "$year" "$link" "$hash" 
+while IFS=';' read -r cont auth title year link hash grading _; do
+  printf -v item '{ "cont": %s,\n "auth": %s,\n "title": %s,\n "year": %s,\ "link": %s,\n "hash": {%s},\n "grading": %s\n }\n' "$cont" "$auth" "$title" "$year" "$link" "$hash" "$grading"
   items+=( "$item" )
-done <'sbb-hackathon - Sheet1.csv'
+done <'sbb-hackathon-2.csv'
 
 IFS=','
 printf '[%s]\n' "${items[*]}" > output.json
